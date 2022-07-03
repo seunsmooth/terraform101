430e4221a1392a4004d94e3674c66182449509fc
seunsmooth <seunsmooth@yahoo.com>
2022-07-03 06:57:49 +0100

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,6 +2,6 @@ module "spin_up_machine" {
     source =  "./mymodules"
     instance_count = 2
     modulescripts  = ["scripts/apache.sh","scripts/tomcat.sh"]
-    node_name = ["application web server ","Application app server"] 
+    node_name = ["Web_server ","App_server"] 
     environment = "dev" 
 }
