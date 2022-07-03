23c728056e360bf7afbf873a41404ea931c44aab
seunsmooth <seunsmooth@yahoo.com>
2022-07-03 06:56:54 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,12 @@
-terraform {
-  backend "s3" {
-    bucket = "techbleat-terraform-state"
-    key    = "dev/terraform.tfstate"
-    region = "eu-west-1"
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "doxa-terraform-state-${random_string.random.result}"
+
+  tags = {
+    Name = "TerraformState "
   }
+}
+resource "random_string" "random" {
+  length  = 8
+  special = false
+  upper   = false
 }
