96f8edfe0ab376823adc9870feecba3ed080169c
seunsmooth <seunsmooth@yahoo.com>
2022-07-03 19:50:19 +0100

    Update build_instance_per_company_spec.tf

--- mymodules/build_instance_per_company_spec.tf
+++ mymodules/build_instance_per_company_spec.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "machine" {
   ami           = "ami-080af029940804103"
   instance_type = "t2.micro"
   count         = var.instance_count
-  key_name      = "techleat2022"
+  key_name      = "terraform22"
 
   user_data =   file(element(var.modulescripts,count.index))
 
